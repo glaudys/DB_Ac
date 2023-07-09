9db6b488e72621a43405e8f1d3bfd326f610f852
glaudys <124433605+glaudys@users.noreply.github.com>
2023-07-09 18:28:35 -0300

    Create script.js

---
+++ .github/workflows/script.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+    // Crie uma cena
+    var scene = new THREE.Scene();
+
+    // Crie uma câmera
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
+
+    // Crie um renderizador
+    var renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('canvas').appendChild(renderer.domElement);
+
+    // Carregue o arquivo PDB
+    var loader = new THREE.PDBLoader();
+    loader.load('seu_arquivo.pdb', function(geometry, atoms) {
+        // Crie um material para o modelo
+        var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+
+        // Crie um objeto de malha
+        var mesh = new THREE.Mesh(geometry, material);
+
+        // Adicione o objeto à cena
+        scene.add(mesh);
+    });
+
+    // Função de renderização
+    function render() {
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
+    }
+
+    // Inicie a renderização
+    render();
+};
